a13df63d0b309182ce28eeb354c8abddb0c9e5d3
Remon Kamal <RemonComputer@gmail.com>
2023-05-01 21:11:50 +0300

    After exporting the habitat agent node to ros2 buut faced a problem while loading the agent model inside the node

--- ros_x_habitat/ros_x_habitat/nodes/habitat_agent_node.py
+++ ros_x_habitat/ros_x_habitat/nodes/habitat_agent_node.py
@@ -382,8 +382,302 @@ class HabitatAgentNode(Node):
         self.declare_parameter('sensor_pub_rate',
                                10.0,
                                sensor_pub_rate_descriptor)
-        self.get_logger().info('Habitat Agent Node Starting...')
-        pass
+        # set up logger
+        self.logger = utils_logging.setup_logger('habitat_agent')
+        self.initialize_threading_utils()
+        self.initialize_agent()
+        self.initialize_services()
+        self.intialize_topics()
+        self.logger.info("agent initialized")
+
+    def initialize_threading_utils(self) -> None:
+        self.get_logger().info('Initailizing threading utils...')
+        self.lock = Lock()
+        # shutdown triggers the node to be shutdown. Guarded by shutdown_cv
+        self.shutdown_cv = Condition()
+        with self.shutdown_cv:
+            self.shutdown = False
+
+    def initialize_agent(self) -> None:
+        self.get_logger().info('Initailizing agent...')
+        self.agent_config = OmegaConf.create({
+            'INPUT_TYPE': self.get_parameter('input_type').
+            get_parameter_value().string_value,
+            'MODEL_PATH': self.get_parameter('model_path').
+            get_parameter_value().string_value,
+            'RANDOM_SEED': 0,
+            'PTH_GPU_ID': 0,
+            'HIDDEN_SIZE': 512,
+        })
+        # lock guarding access to self.action, self.count_steps,
+        # self.agent and self.t_agent_elapsed
+        with self.lock:
+            # the last action produced from the agent
+            self.action = None
+
+            # declare an agent instance
+            self.agent = PPOAgent(self.agent_config)
+
+            # for timing
+            self.count_steps = None
+            self.t_agent_elapsed = 0
+
+    def initialize_services(self) -> None:
+        self.get_logger().info('Initailizing services...')
+        # establish agent reset service server
+        self.reset_service = self.create_service(
+            ResetAgent, ServiceNames.RESET_AGENT,
+            self.reset_agent)
+
+        # establish agent time service server
+        self.agent_time_service = self.create_service(
+            GetAgentTime, ServiceNames.GET_AGENT_TIME,
+            self.get_agent_time)
+        # self.agent_time_service = rospy.Service(
+        #     f"{PACKAGE_NAME}/{self.node_name}/{ServiceNames.GET_AGENT_TIME}",
+        #     GetAgentTime,
+        #     self.get_agent_time,
+        # )
+
+    def intialize_topics(self) -> None:
+        self.get_logger().info('Initailizing topics...')
+        # agent publish and subscribe queue size
+        # TODO: make them configurable by constructor argument
+        self.sub_queue_size = 10
+        self.pub_queue_size = 10
+        # publish to command topics
+        # self.pub = rospy.Publisher(
+        # "action", Int16, queue_size=self.pub_queue_size)
+        self.pub = self.create_publisher(
+            Int16, "action", self.pub_queue_size)
+
+        # subscribe to sensor topics
+        if (
+            self.agent_config.INPUT_TYPE == "rgb"
+            or self.agent_config.INPUT_TYPE == "rgbd"
+        ):
+            self.sub_rgb = message_filters.Subscriber(self, Image, "rgb")
+        if (
+            self.agent_config.INPUT_TYPE == "depth"
+            or self.agent_config.INPUT_TYPE == "rgbd"
+        ):
+            # you might need to replace this by sensor_msgs/msg/PointCloud2
+            # self.sub_depth = message_filters.Subscriber("depth", numpy_msg(DepthImage))
+            self.sub_depth = message_filters.Subscriber(
+                self, DepthImage, "depth")
+        self.sub_pointgoal_with_gps_compass = message_filters.Subscriber(
+            self, PointGoalWithGPSCompass, "pointgoal_with_gps_compass"
+        )
+
+        # filter sensor topics with time synchronizer
+        if self.agent_config.INPUT_TYPE == "rgb":
+            self.ts = TimeSynchronizer(
+                [self.sub_rgb, self.sub_pointgoal_with_gps_compass],
+                queue_size=self.sub_queue_size,
+            )
+            self.ts.registerCallback(self.callback_rgb)
+        elif self.agent_config.INPUT_TYPE == "rgbd":
+            self.ts = TimeSynchronizer(
+                [self.sub_rgb, self.sub_depth, self.sub_pointgoal_with_gps_compass],
+                queue_size=self.sub_queue_size,
+            )
+            self.ts.registerCallback(self.callback_rgbd)
+        else:
+            self.ts = TimeSynchronizer(
+                [self.sub_depth, self.sub_pointgoal_with_gps_compass],
+                queue_size=self.sub_queue_size,
+            )
+            self.ts.registerCallback(self.callback_depth)
+
+        self.logger.info(
+            "agent making sure env subscribed to command topic...")
+#         while self.pub.get_num_connections() == 0:
+#             pass
+        self.get_logger().info('waiting for nodes to connect to this node...')
+        while self.pub.get_subscription_count() == 0:
+            pass
+
+    def reset_agent(self, request):
+        r"""
+        ROS service handler which resets the agent and related variables.
+        :param request: command from the env node.
+        :returns: True
+        """
+        if request.reset == AgentResetCommands.RESET:
+            # reset the agent
+            # NOTE: here we actually re-instantiate a new agent
+            # before resetting it, because somehow PPOAgent.reset()
+            # doesn't work and the agent retains memory from previous
+            # episodes
+            with self.lock:
+                self.count_steps = 0
+                self.t_agent_elapsed = 0.0
+                self.action = None
+
+                self.agent_config.RANDOM_SEED = request.seed
+                OmegaConf.set_readonly(self.agent_config, False)
+                self.agent = PPOAgent(self.agent_config)
+                OmegaConf.set_readonly(self.agent_config, True)
+                self.agent.reset()
+            return True
+        elif request.reset == AgentResetCommands.SHUTDOWN:
+            # shut down the agent node
+            with self.shutdown_cv:
+                self.shutdown = True
+                self.shutdown_cv.notify()
+                return True
+
+    def get_agent_time(self, request):
+        r"""
+        ROS service handler which returns the average time for the agent
+        to produce an action since the last reset.
+        :param request: not used
+        :returns: agent time
+        """
+        with self.lock:
+            avg_agent_time = self.t_agent_elapsed / self.count_steps
+        return avg_agent_time
+
+    def depthmsg_to_cv2(self, depth_msg):
+        r"""
+        Converts a ROS DepthImage message to a Habitat depth observation.
+        :param depth_msg: ROS depth message
+        :returns: depth observation as a numpy array
+        """
+        w = depth_msg.width
+        h = depth_msg.height
+        depth_img = np.reshape(depth_msg.data.astype(np.float32), (h, w))
+        return depth_img
+
+    def msgs_to_obs(
+        self,
+        rgb_msg: Image = None,
+        depth_msg: DepthImage = None,
+        pointgoal_with_gps_compass_msg: PointGoalWithGPSCompass = None,
+    ) -> Dict[str, Any]:
+        r"""
+        Converts ROS messages into Habitat observations.
+        :param rgb_msg: RGB sensor observations packed in a ROS message
+        :param depth_msg: Depth sensor observations packed in a ROS message
+        :param pointgoal_with_gps_compass_msg: Pointgoal + GPS/Compass sensor
+            observations packed in a ROS message
+        :return: Habitat observations
+        """
+        observations = {}
+
+        # Convert RGB message
+        if rgb_msg is not None:
+            observations["rgb"] = (
+                CvBridge().imgmsg_to_cv2(rgb_msg, "passthrough").
+                astype(np.float32)
+            )
+
+        # Convert depth message
+        if depth_msg is not None:
+            observations["depth"] = self.depthmsg_to_cv2(depth_msg)
+            # have to manually add channel info
+            observations["depth"] = np.expand_dims(observations["depth"], 2).\
+                astype(np.float32)
+
+        # Convert pointgoal + GPS/compass sensor message
+        if pointgoal_with_gps_compass_msg is not None:
+            observations["pointgoal_with_gps_compass"] = np.asarray(
+                [
+                    pointgoal_with_gps_compass_msg.distance_to_goal,
+                    pointgoal_with_gps_compass_msg.angle_to_goal,
+                ]
+            ).astype(np.float32)
+
+        return observations
+
+    def action_to_msg(self, action: Dict[str, int]):
+        r"""
+        Converts action produced by Habitat agent to a ROS message.
+        :param action: Discrete action produced by Habitat agent.
+        :returns: A ROS message of action command.
+        """
+        action_id = action["action"]
+        # msg = ...  # type: Union[Twist, Int16]
+        msg = Int16()
+        msg.data = action_id
+
+        return msg
+
+    def callback_rgb(self, rgb_msg, pointgoal_with_gps_compass_msg):
+        r"""
+        Produces an action or velocity command periodically from RGB
+        sensor observation, and publish to respective topics.
+        :param rgb_msg: RGB sensor readings in ROS message format.
+        :param pointgoal_with_gps_compass_msg: Pointgoal + GPS/Compass readings.
+        """
+        # convert current_observations from ROS to Habitat format
+        observations = self.msgs_to_obs(
+            rgb_msg=rgb_msg,
+            pointgoal_with_gps_compass_msg=pointgoal_with_gps_compass_msg,
+        )
+
+        # produce an action/velocity once the last action has completed
+        # and publish to relevant topics
+        with self.lock:
+            # TODO: implement time logging for rgb callback
+            self.action = self.agent.act(observations)
+            self.count_steps += 1
+            action_msg = self.action_to_msg(self.action)
+            self.pub.publish(action_msg)
+
+    def callback_depth(self, depth_msg, pointgoal_with_gps_compass_msg):
+        r"""
+        Produce an action or velocity command periodically from depth
+        sensor observation, and publish to respective topics.
+        :param depth_msg: Depth sensor readings in ROS message format.
+        :param pointgoal_with_gps_compass_msg: Pointgoal + GPS/Compass readings.
+        """
+        # convert current_observations from ROS to Habitat format
+        observations = self.msgs_to_obs(
+            depth_msg=depth_msg,
+            pointgoal_with_gps_compass_msg=pointgoal_with_gps_compass_msg,
+        )
+
+        # produce an action/velocity once the last action has completed
+        # and publish to relevant topics
+        with self.lock:
+            # TODO: implement time logging for depth callback
+            self.action = self.agent.act(observations)
+            self.count_steps += 1
+            action_msg = self.action_to_msg(self.action)
+            self.pub.publish(action_msg)
+
+    def callback_rgbd(self, rgb_msg, depth_msg, pointgoal_with_gps_compass_msg):
+        r"""
+        Produces an action or velocity command periodically from RGBD
+        sensor observations, and publish to respective topics.
+        :param rgb_msg: RGB sensor readings in ROS message format.
+        :param depth_msg: Depth sensor readings in ROS message format.
+        :param pointgoal_with_gps_compass_msg: Pointgoal + GPS/Compass readings.
+        """
+        # convert current_observations from ROS to Habitat format
+        observations = self.msgs_to_obs(
+            rgb_msg=rgb_msg,
+            depth_msg=depth_msg,
+            pointgoal_with_gps_compass_msg=pointgoal_with_gps_compass_msg,
+        )
+
+        # produce an action/velocity once the last action has completed
+        # and publish to relevant topics
+        with self.lock:
+            # ------------ log agent time start ------------
+            t_agent_start = time.clock()
+            # ----------------------------------------------
+
+            self.action = self.agent.act(observations)
+
+            # ------------ log agent time end ------------
+            t_agent_end = time.clock()
+            self.t_agent_elapsed += t_agent_end - t_agent_start
+            self.count_steps += 1
+
+            action_msg = self.action_to_msg(self.action)
+            self.pub.publish(action_msg)
 
 
 def main(args=None):
